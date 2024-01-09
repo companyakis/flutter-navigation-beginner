91bb8d436af4998d170af41e5d92e14257eb00fe
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-01-09 16:21:38 +0300

    Create main_page.dart

---
+++ main_page.dart
@@ -0,0 +1,31 @@
+import 'package:blankp/page_a.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Main Page")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => PageA()));
+                },
+                child: const Text("Page A"))
+          ],
+        ),
+      ),
+    );
+  }
+}
